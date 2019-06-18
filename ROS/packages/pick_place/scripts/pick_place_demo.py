67467d1e5f62fb4ee6d07650e7f9c208f2b47a51
Luke Boorman <l.boorman@sheffield.ac.uk>
2019-06-18 17:09:46 +0100

    Implementing 'Pick and Place' demo

--- ROS/packages/pick_place/scripts/pick_place_demo.py
+++ ROS/packages/pick_place/scripts/pick_place_demo.py
@@ -11,6 +11,8 @@ from sensor_msgs.msg import JointState
 from math import pi, radians, degrees
 from actionlib import SimpleActionClient
 from tf.transformations import *
+import readchar
+import subprocess as sp
 
 class PickPlace():
 
@@ -33,8 +35,18 @@ class PickPlace():
 
 			# Publisher for Kuka API
 			self._kuka_api_pub = rospy.Publisher('/moveit_iiwa', String, queue_size=10)
+
+			self._pose_over_tools = self.create_pose(0.999, -0.000, -0.001, 0.045, 0.365, 0.164, 0.623)
+			self._pose_to_tools = self.create_pose(0.999, -0.001, -0.001, 0.045, 0.365, 0.155, 0.525)
+			self._pose_place_away = self.create_pose(0.999, 0.000, -0.001, 0.045, 0.687, 0.143, 0.385)
+			self._pose_place_table = self.create_pose(0.999, 0.000, -0.001, 0.045, 0.687, 0.143, 0.285)
+			self._pose_home = self.create_pose(-0.000, 0.000, 0.000, 1.000, 0.000, 0.000, 1.306)
+
 		except:
 			print("An error occured. Make sure you are running MoveIt simulation first!")
+			print('Exiting application')
+			rospy.sleep(2)
+			#sys.exit()
 
 	# Moves robots to joint goal
 	def move_to_joint_goal(self, joint_positions):
@@ -45,30 +57,110 @@ class PickPlace():
 
 		self._move_group.stop()
 
+	# Returns the Pose object created from arguments passed in
+	def create_pose(self, *args):
+		print(args)
+		pose = Pose()
+
+		pose.orientation.x = args[0]
+		pose.orientation.y = args[1]
+		pose.orientation.z = args[2]
+		pose.orientation.w = args[3]
+		pose.position.x = args[4]
+		pose.position.y = args[5]
+		pose.position.z = args[6]
+
+		return pose
+
 	# Moves the robot to a pose
 	def move_to_pose(self):
-		pose_goal = Pose()
+		## PICK AND PLACE SEQUENCE
+		## 1. Move over the tools
+		## 2. Open grippers
+		## 3. Move to pick up a tool
+		## 4. Close grippers
+		## 5. Place the tool away
+		## 6. Open grippers
+		## 7. Return to home
 
-		# Setting up RPY so that the end effector is always facing downwards
-		q = quaternion_from_euler(0, pi, 0)
-
-		pose_goal.orientation.x = q[0]
-		pose_goal.orientation.y = q[1]
-		pose_goal.orientation.z = q[2]
-		pose_goal.orientation.w = q[3]
-		pose_goal.position.x = .0
-		pose_goal.position.y = .0
-		pose_goal.position.z = .0
-
-		self._move_group.set_pose_target(pose_goal)
-		try:
-			plan = self._move_group.go(wait=True)
-			print("Moved correctly")
-		except Exception as e:
-			print("Error: %s" % e)
+		print('Moving over the tools')
+		self._move_group.set_pose_target(self._pose_over_tools)
+		self._move_group.go(wait=True)
 		self._move_group.stop()
-
 		self._move_group.clear_pose_targets()
+		self.move_robot()
+		print('I like to move it, move it!')
+
+		print('Openning grippers')
+		#self.open_grippers()
+		#self._move_group.go(wait=True)
+		#self._move_group.stop()
+		#self._move_group.clear_pose_targets()
+		print('Open up my eager eyes!')
+
+		print('Picking up a tool')
+		self._move_group.set_pose_target(self._pose_to_tools)
+		self._move_group.go(wait=True)
+		self._move_group.stop()
+		self._move_group.clear_pose_targets()
+		self.move_robot()
+		print('I got to move like Jagger!')
+
+		print('Closing grippers')
+		#self.close_grippers()
+		#self._move_group.go(wait=True)
+		#self._move_group.stop()
+		#self._move_group.clear_pose_targets()
+		print('Grippers closed')
+
+		print('Moving over the tools')
+		self._move_group.set_pose_target(self._pose_over_tools)
+		self._move_group.go(wait=True)
+		self._move_group.stop()
+		self._move_group.clear_pose_targets()
+		self.move_robot()
+		print('I like to move it, move it!')
+
+		print('Placing away the tool')
+		self._move_group.set_pose_target(self._pose_place_away)
+		self._move_group.go(wait=True)
+		self._move_group.stop()
+		self._move_group.clear_pose_targets()
+		self.move_robot()
+		print('Tool placed away')
+
+		print('Placing away the tool')
+		self._move_group.set_pose_target(self._pose_place_table)
+		self._move_group.go(wait=True)
+		self._move_group.stop()
+		self._move_group.clear_pose_targets()
+		self.move_robot()
+		print('Tool placed away')
+
+		print('Openning grippers')
+		#self.open_grippers()
+		#self._move_group.go(wait=True)
+		#self._move_group.stop()
+		#self._move_group.clear_pose_targets()
+		print('Grippers opened')
+
+		print('Returning home')
+		self._move_group.set_pose_target(self._pose_home)
+		self._move_group.go(wait=True)
+		self._move_group.stop()
+		self._move_group.clear_pose_targets()
+		self.move_robot()
+		print('Nothing feels like home anymore')
+
+	# Moves the robot (in simulation) back to home position
+	def move_to_home(self):
+		print('Returning home')
+		self._move_group.set_pose_target(self._pose_over_tools)
+		self._move_group.go(wait=True)
+		self._move_group.stop()
+		self._move_group.clear_pose_targets()
+		self.move_robot()
+		print('Nothing feels like home anymore')
 
 	# Moves the robot to save position, defined in MoveIt! Config
 	def move_to_saved_position(self, name):
@@ -93,6 +185,19 @@ class PickPlace():
 
 	# Sends the command to Kuka API to move the actual robot
 	def move_robot(self):
+		msg_to_send = "setPosition "
+		joint_positions_string = ""
+		joint_positions = self._move_group.get_current_joint_values()
+		for x in joint_positions:
+			joint_positions_string += str(math.degrees(x)) + " "
+		msg_to_send += joint_positions_string
+		msg_to_send.strip()
+		print(msg_to_send)
+		rospy.sleep(1)
+
+		self._kuka_api_pub.publish(msg_to_send)
+		print(self._kuka_api_pub)
+		rate.sleep()
 		pass
 
 	# Gets the joint state from the robot
@@ -120,29 +225,53 @@ class PickPlace():
 		# Unregisteres from the topic
 		self._join_state_sub.unregister()
 
-def display_menu():
+def display_menu(pick_place):
 	print('\n\n\n')
-	print('*'*30)
-	print('*'*30)
-	print('**'+' '*26+'**')
-	print('**'+' '*26+'**')
-	print('**'+' '*7+'RAS HACKATHON'+' '*6+'**')
-	print('**'+' '*26+'**')
-	print('**'+' '*26+'**')
-	print('*'*30)
-	print('*'*30)
+	print('*'*40)
+	print('*'*40)
+	print('**'+' '*36+'**')
+	print('**'+' '*36+'**')
+	print('**'+' '*12+'RAS HACKATHON'+' '*11+'**')
+	print('**'+' '*36+'**')
+	print('**'+' '*36+'**')
+	print('*'*40)
+	print('*'*40)
 	print('1. Show `Pick and Place` Demonstration')
 	print('2. Reset robot to Home Position')
 	print('3. Move robot to XYZ Position')
 	print('4. Move robot to saved position')
 	print('5. Exit')
+	print('What do you want to do?')
+	try:
+		choice = int(readchar.readchar())
+	except:
+		print('Only numbers allowed')
+	
+	while (choice > 5):
+		print('Please choose one of the valid options [1-5]')
+		try:
+			choice = int(readchar.readchar())
+		except:
+			print('Only numbers allowed')
 
-
+	if choice == 5:
+		print('Closing an application')
+		sys.exit()
+	elif choice == 4:
+		pass
+	elif choice == 3:
+		pass
+	elif choice == 2:
+		pick_place.move_to_home()
+		pass
+	elif choice == 1:
+		pick_place.move_to_pose()
+		pass
 
 if __name__ == '__main__':
 	pick_place = PickPlace()
 	rospy.sleep(2)
-	display_menu()
+	display_menu(pick_place)
 	#pick_place.set_robots_initial_state()
 	#rospy.sleep(2)
 	#pick_place.move_to_pose()
